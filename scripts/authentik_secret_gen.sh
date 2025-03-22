9faf5d290a883a159d4d68dbcb8742efb73bcb26
Chris Lansing <cslansin@gmail.com>
2025-03-22 11:23:59 -0400

    Move postgres and redis into authentik compose file

--- scripts/authentik_secret_gen.sh
+++ scripts/authentik_secret_gen.sh
@@ -1,4 +1,3 @@
-openssl rand -base64 36 | tr -d '\n' | sudo tee postgres_default_password
-echo "authentik_db_user" | sudo tee authentik_postgresql_user
+echo "$AUTHENTIK_POSTGRESQL__USER" | sudo tee authentik_postgresql_user
 openssl rand -base64 60 | tr -d '\n' | sudo tee authentik_postgresql_password
 openssl rand -base64 60 | tr -d '\n' | sudo tee authentik_secret_key
